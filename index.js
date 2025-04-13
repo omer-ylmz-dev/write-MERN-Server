4fe6add04ebf4e6b0f23cb7c34e3b27ce9ab3b2b
omer-ylmz-dev <y.omer96@gmail.com>
2025-04-14 02:20:41 +0400

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,26 @@ import noteRouter from "./routes/note.js";
 import userRouter from "./routes/user.js";
 
 app.use(cors({
-	origin:process.env.CLIENT_URL,
-	methods: ["GET","POST","PUT","DELETE"],
-	credentials:true
+	origin: process.env.CLIENT_URL,
+	methods: ["GET", "POST", "PUT", "DELETE"],
+	credentials: true
 }));
 
-app.use(bodyParser.json({limit:"30mb", extended:true}));
-app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-app.use("/",noteRouter);
-app.use("/",userRouter);
+app.use("/", noteRouter);
+app.use("/", userRouter);
+
+
+app.get("/", (req, res) => {
+	res.send("Server is working");
+});
 
 
 
 dbConnection();
 
 app.listen(process.env.PORT, () => {
-	console.log("Server is running on ",process.env.PORT)
+	console.log("Server is running on ", process.env.PORT)
 })
